70519cbfda831e0f19b832ece61d0ef4142e3bf8
PC <belyevevgen@mail.ru>
2020-11-22 22:07:41 +0300

    add new file

---
+++ tabl_elem_row.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8" />
+<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/nav.css">
+<link rel="stylesheet" href="css/swiper-bundle.min.css">
+<script src="js/jquery-3.4.1.min.js"></script>
+<title></title>
+</head>
+<body>
+<?php $a=1; include 'header_nav.php'; ?>
+<section >
+   <h1>Строка таблицы</h1>
+</section>
+<?php include 'footer_nav.php'; ?>
+</body>
+<script src="js/swiper-bundle.min.js"></script>
+<script src="js/main.js"></script>
+</html>
