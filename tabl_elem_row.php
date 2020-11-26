92dde4bc2549a923760718235261445afe6f297b
PC <belyevevgen@mail.ru>
2020-11-26 21:44:49 +0300

    текущие корректировки

--- tabl_elem_row.php
+++ tabl_elem_row.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <meta charset="utf-8" />
-<link rel="stylesheet" href="css/main.css">
+<link rel="stylesheet" href="css/tabl_elem_row.css">
 <link rel="stylesheet" href="css/nav.css">
 <link rel="stylesheet" href="css/swiper-bundle.min.css">
 <script src="js/jquery-3.4.1.min.js"></script>
@@ -10,11 +10,31 @@
 </head>
 <body>
 <?php $a=1; include 'header_nav.php'; ?>
-<section >
-   <h1>Строка таблицы1</h1>
+<section id="mainblok">
+	<div id="leftblok">
+		<div id="ident">
+			Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tenetur beatae commodi dicta facilis, iure aliquid vitae officiis magni explicabo nam, debitis id praesentium. Ut minima corporis iure tempora fuga perferendis adipisci sequi, optio a quibusdam voluptatibus assumenda ex? Temporibus laboriosam ea at, nisi odio, quos perspiciatis officia ut quaerat porro, obcaecati praesentium, blanditiis laudantium! Quidem animi totam excepturi sint laborum perspiciatis ut enim est? Molestiae alias beatae dolor aut aspernatur quia architecto in quibusdam nesciunt, laborum similique omnis, quos, accusamus nam quod debitis atque quidem soluta sint veniam. Esse sint beatae quaerat provident dolore, voluptatem consequatur vitae expedita, quia possimus?
+		</div>
+		<div id="foto">
+			
+		</div>
+		<div id="prg">
+			
+		</div>
+	</div>
+	<div id="rightblok">
+		<div id="instruk">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi maiores perferendis, amet nemo ut minima, minus assumenda vero sunt harum culpa rem repellat, optio voluptas accusamus eligendi, quisquam placeat recusandae ratione facere quo voluptatibus delectus! Culpa nihil nisi dicta eius quasi vero magnam itaque eos eligendi perspiciatis commodi, obcaecati a?
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque aut, earum magni delectus assumenda aperiam placeat, veniam. Quos odit magni cupiditate laudantium at incidunt aliquid nulla quis! Neque odit a reiciendis quibusdam earum aperiam qui fugiat illo mollitia maiores omnis minima iste recusandae praesentium nam, dolorem libero culpa aspernatur tempore eius laudantium enim officia. Molestiae quasi id vitae nisi autem hic rerum sint accusantium, natus illo aliquid laborum voluptatibus porro quisquam, dolorem quibusdam, debitis officiis. Modi itaque saepe dolorum exercitationem iste praesentium molestias corporis quidem voluptatum est debitis quos perferendis accusantium harum quis, expedita adipisci quia! Et ratione officiis eveniet explicabo ab natus tenetur aliquam omnis debitis, vitae laudantium illo reprehenderit, expedita suscipit repellat sit labore tempore sint provident ipsum tempora quod, id assumenda ad eum! Quod unde blanditiis harum officia, enim, nulla vitae ratione facilis iusto illo quibusdam iste rerum nobis non nam cum sed laboriosam sit saepe, quis explicabo neque! Perspiciatis, nihil culpa fugiat, facilis excepturi voluptas voluptatem neque, ea deleniti temporibus esse laborum beatae! Sed, reprehenderit nisi? Aliquam culpa quam incidunt cum voluptatum consequatur recusandae eius, sit sapiente error saepe voluptatibus excepturi modi quibusdam inventore quidem illum deserunt aperiam? Voluptatem, dolores id, repudiandae rerum quas repellendus corrupti?	
+		</div>
+		<div id="sxema">
+			
+		</div>
+	</div>
+<!--   <h1>Строка таблицы1</h1>-->
 </section>
 <?php include 'footer_nav.php'; ?>
 </body>
 <script src="js/swiper-bundle.min.js"></script>
-<script src="js/main.js"></script>
+<script src="js/tabl_elem_row.js"></script>
 </html>
