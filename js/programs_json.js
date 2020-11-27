6581c6ff42bfd91db3c3855b842f1e47228fa2f1
work <belyevevgen@mail.ru>
2020-11-27 14:30:47 +0300

    fetch 1

---
+++ js/programs_json.js
@@ -0,0 +1,58 @@
+window.addEventListener("load", function(){
+  $('#mbutton').click(async function(){ 
+//    alert('Pres Button');
+
+/*
+let response = await fetch('/article/fetch/logo-fetch.svg');
+
+let blob = await response.blob(); // скачиваем как Blob-объект
+
+// создаём <img>
+let img = document.createElement('img');
+img.style = 'position:fixed;top:10px;left:10px;width:100px';
+document.body.append(img);
+
+// выводим на экран
+img.src = URL.createObjectURL(blob);
+
+setTimeout(() => { // прячем через три секунды
+  img.remove();
+  URL.revokeObjectURL(img.src);
+}, 3000);
+*/
+//let res = fetch('http://www.mocky.io/v2/5944e07213000038025b6f30')
+/*
+let res = fetch('filejson/exp001.json')
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+  });
+//alert('console.log(res)');
+let sjson=JSON.parse(res);
+alert(sjson[color]);
+*/
+//let url='http://www.mocky.io/v2/5944e07213000038025b6f30';
+let url='filejson/exp001.json';
+let response = await fetch(url);
+
+if (response.ok) { // если HTTP-статус в диапазоне 200-299
+  // получаем тело ответа (см. про этот метод ниже)
+  let json = await response.json();
+  console.log("Ok ");
+  console.log(json);
+  console.log(json.array[0]);
+  console.log(json.color);
+  console.log(json.boolean);
+  console.log(json.number);
+  console.log(json.string);
+  console.log(json.object.a);
+  console.log(json.color);
+
+} else {
+  alert("Ошибка HTTP: " + response.status);
+}
+
+  });
+}); 
---
+++ programs_json.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="css/nav.css">
+<link rel="stylesheet" href="css/programs_json.css">
+<script src="js/jquery-3.4.1.min.js"></script>
+<title>Document</title>
+</head>
+<body>
+<?php $a=1; include 'header_nav.php'; ?>
+<section>
+<h1>Проверка работы FETCH</h1>	
+<button id="mbutton">Выполнить</button>
+<div>
+<img src="foto_ali/arduino_uno.jpg" width="500px" height="500px">
+</div>
+</section>
+<?php include 'footer_nav.php'; ?>	
+</body>
+<script src="js/programs_json.js"></script>
+</html>
